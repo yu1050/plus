06848e1e116f45e4aed263343656ea1579a32b3f
Frank <yu1050@gmail.com>
2024-04-28 16:05:47 -0400

    CCC

--- ch02/skip_add.cpp
+++ ch02/skip_add.cpp
@@ -6,6 +6,7 @@
 #include <sstream>
 
 using namespace std;                      // make definitions visible
+
 void readInts(vector<int> *p) {
     string input;
     getline(cin, input);
@@ -15,7 +16,7 @@ void readInts(vector<int> *p) {
 }
 
 
-int skipAdd(int *nums, int length, int k0, int k1) {
+int skipAdd1(int *nums, int length, int k0, int k1) {
     int total = 0;
     for (int i = 0; i < length; i++) {
         if (i < k0 || i > k1) {
@@ -25,9 +26,26 @@ int skipAdd(int *nums, int length, int k0, int k1) {
     return total;
 }
 
+
+int skipAdd2(int *nums, int length, int k0, int k1) {
+    int total = 0;
+    int
+            num11[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,};
+    for (int i = 0; i < k0; i++) {
+        num11[nums[i]]++;
+    }
+    for (int i = k1 + 1; i < length; i++) {
+        num11[nums[i]]++;
+    }
+    for (int i = 1; i < 11; ++i) {
+        total += num11[i] * i;
+    }
+    return total;
+}
+
 int main() {
+
     vector<int> temp;
-    vector<int> result;
     int count, round;
     readInts(&temp);
     count = temp.at(0);
@@ -35,23 +53,25 @@ int main() {
 
     temp.clear();
     readInts(&temp);
-
     int len = temp.size();
+
     int nums[len];
     std::copy(temp.begin(), temp.end(), nums);
 
     for (int i = 0; i < round; i++) {
+
         temp.clear();
         readInts(&temp);
         int k0, k1;
         k0 = temp.at(0);
         k1 = temp.at(1);
 
-        int value = skipAdd(nums, len, k0, k1);
+        int value = skipAdd2(nums, len, k0 - 1, k1 - 1);
 
-        result.push_back(value);
+        std::cout << value << endl;
     }
-    for (int i: result)
-        std::cout << i << ' ';
+
+    cout << endl;
+
     return 0;
 }
